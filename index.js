744df92b6fa7c2088ac5eab5a0a551e415fb9f6a
VazquezAlex <alejandrovazquez.dev@gmail.com>
2023-12-27 14:24:49 -0600

    feature: set up express server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('My App');
+// Third-party imports.
+const express = require('express');
+
+// Create express app.
+const app = express();
+
+// Port to run at.
+const port = 3005;
+
+app.get('/', (req, res) => {
+    res.send('Hello, express listening');
+});
+
+// Setup server port.
+app.listen(port, () => {
+    console.log('Server running on ', port);
+});
