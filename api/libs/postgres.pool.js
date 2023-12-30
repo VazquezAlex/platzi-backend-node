b31d494e5cc7bd2a7d7a9e0c4ed54e040408a083
VazquezAlex <alejandrovazquez.dev@gmail.com>
2023-12-29 21:05:31 -0600

    feature: updated connection to connection string

--- api/libs/postgres.pool.js
+++ api/libs/postgres.pool.js
@@ -1,15 +1,22 @@
 // Third-party imports.
 const { Pool } = require('pg');
 
+const USER = encodeURIComponent(process.env.DB_USER);
+const PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
+const URI = `postgres://${USER}:${PASSWORD}@${ process.env.DB_HOST }:${ process.env.DB_PORT }/${ process.env.DB_NAME }`;
+
+const pool = new Pool({ connectionString: URI })
+
 /**
  * Creates a connection pool to postgres.
+ * This is an explicit connection, were we specified each item.
 */
-const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+// const pool = new Pool({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
 
 module.exports =  pool;
