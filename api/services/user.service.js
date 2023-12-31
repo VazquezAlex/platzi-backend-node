407683dd57dbbd01f0f26be86e8092077e0a90cd
VazquezAlex <alejandrovazquez.dev@gmail.com>
2023-12-30 23:38:57 -0600

    feature: added crud operations for user

--- api/services/user.service.js
+++ api/services/user.service.js
@@ -8,31 +8,41 @@ const { models } = require('./../libs/sequelize');
 class UserService {
   constructor() {}
 
-  async create(data) {
-    return data;
-  }
+    async create(data) {
+        const res = await models.User.create(data);
+        return res;
+    }
 
-  async find() {
-    // const client = await getConnection();
-    // const rta = await client.query('SELECT * FROM tasks');
-    const rta = await models.User.findAll();
-    return rta;
-  }
+    async find() {
+        // const client = await getConnection();
+        // const rta = await client.query('SELECT * FROM tasks');
+        const rta = await models.User.findAll();
+        return rta;
+    }
 
-  async findOne(id) {
-    return { id };
-  }
+    async findOne(id) {
+        const user = await models.User.findByPk(id);
+        if (!user) throw boom.notFound('user not found');
 
-  async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
-  }
+        return user;
+    }
 
-  async delete(id) {
-    return { id };
-  }
+    async update(id, changes) {
+        // Get the user.
+        const user = await this.findOne(id);
+
+        // Update the user.
+        const res = user.update(changes);
+
+        return res;
+    }
+
+    async delete(id) {
+        const user = await this.findOne(id);
+        await user.destroy();
+
+        return id;
+    }
 }
 
 module.exports = UserService;
