dd0ebc0e8dac308230a2b8c9bc82faa9c4592352
VazquezAlex <alejandrovazquez.dev@gmail.com>
2024-01-04 15:07:00 -0600

    feature: added relationship between categories and products

--- api/db/models/index.js
+++ api/db/models/index.js
@@ -1,14 +1,20 @@
 // Local imports.
 const { Customer, CustomerSchema } = require('./customer.model');
+const { Category, CategorySchema } = require('./category.model');
+const { Product, ProductSchema } = require('./product.model');
 const { User, UserSchema } = require('./user.model');
 
 function setupModels(sequelize) {
     User.init(UserSchema, User.config(sequelize));
     Customer.init(CustomerSchema, Customer.config(sequelize));
+    Category.init(CategorySchema, Category.config(sequelize));
+    Product.init(ProductSchema, Product.config(sequelize));
 
     // Run associations (to set relationships).
     User.associate(sequelize.models);
     Customer.associate(sequelize.models);
+    Category.associtate(sequelize.models);
+    Product.associtate(sequelize.models);
 }
 
 module.exports = setupModels;
--- api/schemas/product.schema.js
+++ api/schemas/product.schema.js
@@ -3,11 +3,13 @@ const Joi = require('joi');
 const id = Joi.string().uuid();
 const name = Joi.string().min(3).max(20);
 const price = Joi.number().positive().integer();
+const description = Joi.string();
 const image = Joi.string().uri();
 
 const createProductSchema = Joi.object({
     name: name.required(),
     price: price.required(),
+    description: description.required(),
     image: image.required(),
 });
 
@@ -15,6 +17,7 @@ const updateProductSchema = Joi.object({
     name: name,
     price: price,
     image: image,
+    description: description,
 });
 
 const getProductSchema = Joi.object({
