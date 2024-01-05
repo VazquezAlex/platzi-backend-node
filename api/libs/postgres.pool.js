7e3c5451a890a43e0ccb99294b3493324abc881d
VazquezAlex <alejandrovazquez.dev@gmail.com>
2024-01-04 23:26:21 -0600

    feature: added new SSL configuration

--- api/libs/postgres.pool.js
+++ api/libs/postgres.pool.js
@@ -1,11 +1,22 @@
 // Third-party imports.
+const { config } = require('dotenv');
 const { Pool } = require('pg');
 
 const USER = encodeURIComponent(process.env.DB_USER);
 const PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
 const URI = `postgres://${USER}:${PASSWORD}@${ process.env.DB_HOST }:${ process.env.DB_PORT }/${ process.env.DB_NAME }`;
 
-const pool = new Pool({ connectionString: URI })
+const options = {
+    connectionString: URI,
+}
+
+if (config.isProd) {
+    options.ssl = {
+        rejectUnauthorized: false
+    }
+}
+
+const pool = new Pool(options);
 
 /**
  * Creates a connection pool to postgres.
