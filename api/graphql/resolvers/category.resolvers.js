b1e0fbddb4b04060491d64547e426191b5f04218
VazquezAlex <alejandrovazquez.dev@gmail.com>
2024-01-12 17:17:21 -0600

    feature: validate user has a valid jwt when sending the request

--- api/graphql/resolvers/category.resolvers.js
+++ api/graphql/resolvers/category.resolvers.js
@@ -1,3 +1,5 @@
+// Third-party imports.
+const boom = require('@hapi/boom');
 
 // Local imports.
 const CategoryService = require('./../../services/category.service');
@@ -5,10 +7,13 @@ const CategoryService = require('./../../services/category.service');
 // Create instance of category service.
 const service = new CategoryService();
 
-const addCategory = (_, { dto }) => {
+const addCategory = async (_, { dto }, context) => {
+    const { user } = await context.authenticate('jwt', { session: false });
+
+    // We validate the user has a valid token.
+    if (!user) throw boom.unauthorized('Your token is invalid.');
 
     return service.create(dto);
-
 }
 
 module.exports = {
